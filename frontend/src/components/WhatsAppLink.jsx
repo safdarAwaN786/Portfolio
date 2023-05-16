21f131c6c5421555fad268409de9866b39be6c4e
Safdar Awan <safdarawan2003@gmail.com>
2023-05-17 01:00:47 +0500

    simple whatsapp

--- frontend/src/components/WhatsAppLink.jsx
+++ frontend/src/components/WhatsAppLink.jsx
@@ -6,19 +6,10 @@ export default function WhatsAppLink(){
     const phoneNumber = "+923244679807"
     const message = "Hi, I would like to chat with you."
 
-    const openWhatsapp = () => {
-        const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-      
-        let url;
-        if (isMobile) {
-          url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-        } else {
-          url = `https://web.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
-        }
-      
-        const target = isMobile ? "_blank" : "whatsapp_web_window";
-        window.open(url, target);
-      };
+    const openWhatsapp = ()=>{
+      const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+      window.open(url);
+  }
 
     return (
         <div className="whatsappLinkBox animate__animated animate__bounceInDown  p-sm-3 " onClick={openWhatsapp}>
